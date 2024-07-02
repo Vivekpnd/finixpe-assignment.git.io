d532c1dae055bc922120fe1bf7cd21b95232e560
Vivek kumar <155221741+Vivekpnd@users.noreply.github.com>
2024-07-02 22:03:13 +0530

    adding assignment.js

---
+++ assignment.js
@@ -0,0 +1,47 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+   var slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) { slideIndex = 1 }
+   slides[slideIndex - 1].style.display = "block";
+   setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+// scroll reveal/////
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay:400
+   });
+   ScrollReveal().reveal('.main-tag', { delay: 500, origin: 'left'});
+   ScrollReveal().reveal('.block-description1', { delay: 500, origin: 'left'});
+   ScrollReveal().reveal('.blockimg1', { delay: 500, origin: 'right'});
